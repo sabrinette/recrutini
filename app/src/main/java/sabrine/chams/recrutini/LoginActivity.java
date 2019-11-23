52364f22338919ce153165ef7f24155987e6c025
AmineBarboura <amine.barboura98@gmail>
2019-11-23 18:02:30 +0100

    beginning of login code to make requests to the webservices.

--- app/src/main/java/sabrine/chams/recrutini/LoginActivity.java
+++ app/src/main/java/sabrine/chams/recrutini/LoginActivity.java
@@ -5,15 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView goRegiter ;
+    TextInputEditText email;
+    TextInputEditText password;
+    TextView goRegiter;
+    Button confirmLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmLogin = findViewById(R.id.confirm_login);
+        confirmLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailText = email.getText().toString();
+                String passwordText = password.getText().toString();
+
+            }
+        });
         goRegiter = findViewById(R.id.go_to_register);
         goRegiter.setOnClickListener(new View.OnClickListener() {
             @Override
