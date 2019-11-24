9807fd88807aaad77f871ac1a9e443dc8e238d0b
sabrinette <sabrine.benalaya@supcom.tn>
2019-11-24 19:04:57 +0100

    offreForm

---
+++ app/src/main/java/sabrine/chams/recrutini/AjoutOffre.java
@@ -0,0 +1,14 @@
+package sabrine.chams.recrutini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutOffre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_offre);
+    }
+}
