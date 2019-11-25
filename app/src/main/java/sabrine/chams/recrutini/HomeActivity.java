37a801281980da844ef9a597f3dea6705e7fa830
AmineBarboura <amine.barboura98@gmail>
2019-11-25 18:52:10 +0100

    list view integrated and not tested yet

--- app/src/main/java/sabrine/chams/recrutini/HomeActivity.java
+++ app/src/main/java/sabrine/chams/recrutini/HomeActivity.java
@@ -1,19 +1,71 @@
 package sabrine.chams.recrutini;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class HomeActivity extends AppCompatActivity {
 
+    ConstraintLayout homePage;
     ListView lst;
-    int[] offerIDs;
+    String[] offerNames;
+    String[] offerDescriptions;
+    String[] offerImgsUrls;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         lst = findViewById(R.id.listview);
+        homePage = findViewById(R.id.home_page);
+        final Activity thisActivity = this;
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, "http://sabrine-chams.alwaysdata.net/get_offre.php", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        int i = 0;
+                        for (Iterator it = response.keys(); it.hasNext();)
+                        {
+                            try {
+                                offerNames[i] = response.optJSONObject((String)it.next()).getString("nom");
+                                offerDescriptions[i] = response.optJSONObject((String)it.next()).getString("description");
+                                offerImgsUrls[i] = response.optJSONObject((String)it.next()).getString("img_url");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            ListViewAdapter listViewAdapter = new ListViewAdapter(thisActivity,offerNames,offerDescriptions,offerImgsUrls);
+                            lst.setAdapter(listViewAdapter);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Snackbar.make(homePage, "Cannot get Offers from server", Snackbar.LENGTH_LONG).show();
+                    }
+                });
+        queue.add(jsonRequest);
 
     }
 }
--- app/src/main/java/sabrine/chams/recrutini/ListViewAdapter.java
+++ app/src/main/java/sabrine/chams/recrutini/ListViewAdapter.java
@@ -2,6 +2,8 @@ package sabrine.chams.recrutini;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -10,6 +12,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class ListViewAdapter extends ArrayAdapter<String> {
     private String[] offernames;
@@ -28,7 +33,33 @@ public class ListViewAdapter extends ArrayAdapter<String> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View r = convertView;
+        ViewHolder viewHolder = null;
+        if (r == null)
+        {
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r = layoutInflater.inflate(R.layout.listview_layout, null, true);
+            viewHolder = new ViewHolder(r);
+            r.setTag(viewHolder);
+        }
+        else
+            viewHolder = (ViewHolder) r.getTag();
+        viewHolder.name.setText(offernames[position]);
+        viewHolder.description.setText(descriptions[position]);
+        viewHolder.img.setImageDrawable(loadImageFromUrl(imgUrls[position],Integer.toString(position)));
+        return r;
+    }
+
+    private Drawable loadImageFromUrl(String url, String img_id)
+    {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, img_id);
+            return d;
+        } catch (Exception e)
+        {
+            return null;
+        }
     }
     class ViewHolder{
         TextView name;
